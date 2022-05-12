9489a577724054dc0de78b2d3d0e22327f30aebe
Verminfate <72428571+Verminfate@users.noreply.github.com>
2022-05-12 12:42:18 -0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int lines = 0;
+    string text;
+    string name;
+    cout << "Input name of exe to convert or path including .exe\n>";
+    cin >> name;
+    string cmd = "certutil -encode " + name + " output.txt";
+    system(cmd.c_str());
+    ofstream source_file("source.cpp");
+    source_file << "#include <iostream>"<<endl;
+    source_file << "#include <fstream>" << endl;
+    source_file << "using namespace std;" << endl;
+    source_file << "int main()" << endl;
+    source_file << "{" << endl;
+    source_file << "    ofstream output(\"cvrt.txt\");" << endl;
+    ifstream txtfile("output.txt");
+    while (!txtfile.eof())
+    {
+        getline(txtfile, text);
+        source_file << "    output <<\"" + text + "\"<<endl;" << endl;
+    }
+    source_file << "    output.close();" << endl;
+    source_file << "    system(\"certutil -decode cvrt.txt output.exe\");" << endl;
+    source_file << "    system(\"output.exe\");" << endl;
+    source_file << "}";
+    source_file.close();
+    return 0;
+}
+
